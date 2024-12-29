95b3f8bc92dd555114a1e974bebf3aa07d7ca677
Your New Name <mahamudul408432@gmail.com>
2024-12-29 11:07:29 +0600

    Add relationships to Task model for User and Category

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Task extends Model
 {
-    //
+    protected $fillable = ['title', 'description', 'due_date'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
 }
